48e036a4e6b3661d271a82d4f3120093df624d8a
GPC <anthonytguilherme@hotmail.com>
2022-08-23 15:07:00 -0300

    refactoring

--- js/main.js
+++ js/main.js
@@ -1,43 +1,41 @@
-const form = document.getElementById('novoItem')
-const lista = document.getElementById('lista')
-const itens = JSON.parse(localStorage.getItem('itens')) || []
+const form = document.getElementById("novoItem")
+const lista = document.getElementById("lista")
+const itens = JSON.parse(localStorage.getItem("itens")) || []
 
-itens.forEach( (elemento) => {
-    console.log(elemento.nome, elemento.quantidade)
-})
+itens.forEach( (elemento) => {    
+    criaElemento(elemento)
+} )
 
-form.addEventListener('submit', (evento) =>{
-    evento.preventDefault()
-    
+form.addEventListener("submit", (evento) => {   
+    evento.preventDefault()            
+
     const nome = evento.target.elements['nome']
     const quantidade = evento.target.elements['quantidade']
 
-    criaElemento(nome.value, quantidade.value)
-
-    nome.value = ''
-    quantidade.value = ''
-
-})
-
-function criaElemento(nome, quantidade){
-    const novoItem = document.createElement('li')
-    novoItem.classList.add('item')
-
-    const numeroItem = document.createElement('strong')
-    numeroItem.innerHTML = quantidade
-    
-    novoItem.appendChild(numeroItem)
-    novoItem.innerHTML += nome
-
-    lista.appendChild(novoItem)
-
     const itemAtual = {
-        'nome': nome,
-        'quanttidade': quantidade
+    "nome": nome.value,
+    "quantidade": quantidade.value
     }
+
+    criaElemento(itemAtual)
 
     itens.push(itemAtual)
 
-    localStorage.setItem('item', JSON.stringify(itens))
+    localStorage.setItem("itens", JSON.stringify(itens))
 
+    nome.value = ""
+    quantidade.value = ""
+})
+
+function criaElemento(item) {  
+    const novoItem = document.createElement('li')
+    novoItem.classList.add("item")
+
+    const numeroItem = document.createElement('strong')
+    numeroItem.innerHTML = item.quantidade
+    novoItem.appendChild(numeroItem)
+
+    novoItem.innerHTML += item.nome
+
+    lista.appendChild(novoItem)
 }
