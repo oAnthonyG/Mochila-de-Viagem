9ddd3117cfdb09e9a59298d6fff9bf3577d154a8
GPC <anthonytguilherme@hotmail.com>
2022-08-24 15:24:45 -0300

    create the function atualizaElemento to update the element

--- js/main.js
+++ js/main.js
@@ -12,14 +12,26 @@ form.addEventListener("submit", (evento) => {
     const nome = evento.target.elements['nome']
     const quantidade = evento.target.elements['quantidade']
 
+    const existe = itens.find(elemento = elemento => elemento.nome === nome.value)
     const itemAtual = {
-    "nome": nome.value,
-    "quantidade": quantidade.value
+        "nome": nome.value,
+        "quantidade": quantidade.value
+        }
+
+    if(existe){
+        itemAtual.id = existe.id
+        atualizaElemento(itemAtual)
+    } else{
+        itemAtual.id = itens.length
+
+        criaElemento(itemAtual)
+
+        itens.push(itemAtual)
     }
 
-    criaElemento(itemAtual)
+    
 
-    itens.push(itemAtual)
+    
 
     localStorage.setItem("itens", JSON.stringify(itens))
 
@@ -33,9 +45,14 @@ function criaElemento(item) {
 
     const numeroItem = document.createElement('strong')
     numeroItem.innerHTML = item.quantidade
+    numeroItem.dataset.id = item.id
     novoItem.appendChild(numeroItem)
 
     novoItem.innerHTML += item.nome
 
     lista.appendChild(novoItem)
+}
+
+function atualizaElemento(item){
+    document.querySelector("[data-id='"+item.id+"']").innerHTML = item.quantidade
 }
