cf4ca16d799fa44761e3597da53258114c167dde
GPC <anthonytguilherme@hotmail.com>
2022-08-24 15:02:34 -0300

    fixed some bugs

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
-const form = document.getElementById("novoItem")
+const form = document.getElementById("novoItem") 
 const lista = document.getElementById("lista")
-const itens = JSON.parse(localStorage.getItem("itens")) || []
+const itens = JSON.parse(localStorage.getItem("itens")) || []  
 
 itens.forEach( (elemento) => {    
     criaElemento(elemento)
 } )
 
-form.addEventListener("submit", (evento) => {   
-    evento.preventDefault()            
+form.addEventListener("submit", (evento) => {  
+    evento.preventDefault()
 
     const nome = evento.target.elements['nome']
     const quantidade = evento.target.elements['quantidade']
